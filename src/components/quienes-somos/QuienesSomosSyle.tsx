07adc88bb8490be8fa598bb8a569068303d99012
Fabricio Salgado <fabricio@5e.cr>
2023-10-24 11:47:11 -0600

    responive on quienes-somos

--- src/components/quienes-somos/QuienesSomosSyle.tsx
+++ src/components/quienes-somos/QuienesSomosSyle.tsx
@@ -13,13 +13,24 @@ export const QuienesSomosStyle = styled.section`
     background-size: cover;
     background-position: top;
     margin-bottom: -15rem;
+    @media ${DEVICE.maxmd} {
+      height: 50rem;
+      background-position: 70% 100%;
+      margin-bottom: -30rem;
+    }
   }
   .wordpress-content {
     width: 84.4375rem;
     margin: auto;
     padding: 1rem 16rem 1rem 0;
+    @media ${DEVICE.maxmd} {
+      width: 100%;
+      padding: 0 2rem 5rem;
+    }
     h1 {
-      font-size: 4rem;
+      @media ${DEVICE.md} {
+        font-size: 4rem;
+      }
       margin-bottom: 3rem;
     }
     h2 {
@@ -46,11 +57,19 @@ export const QuienesSomosStyle = styled.section`
       color: ${COLORS.white};
     }
 
-    li {
-      &::marker { 
-        color: ${COLORS.blue};
+    ul {
+      @media ${DEVICE.maxxs} {
+        padding: 1rem 1.5rem;
       }
-      padding: 1.5rem 0;
+      li {
+        &::marker { 
+          color: ${COLORS.blue};
+        }
+        padding: 1.5rem 0;
+        @media ${DEVICE.maxxs} {
+          padding: 1rem 0;
+        }
+      }
     }
 
   }
