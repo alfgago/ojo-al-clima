2d19ef4d311c7ed1e68f9c840d33475ec7913081
Fabricio Salgado <fabricio@5e.cr>
2023-11-16 08:35:43 -0600

    added  social links

--- src/components/share/index.tsx
+++ src/components/share/index.tsx
@@ -1,41 +1,16 @@
-import { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
-import {
-  FacebookShareButton,
-  TwitterShareButton,
-  RedditShareButton,
-  WhatsappShareButton,
-} from 'react-share';
 import { ReactSVG } from 'react-svg';
 import { ShareStyle } from './ShareStyle';
 
 export const Share = () => {
-  const router = useRouter();
-  const [pageTitle, setPageTitle] = useState('Ojo al clima');
-
-  const shareUrl = `${process.env.NEXT_PUBLIC_VERCEL_URL || 'https://ojo-al-clima.vercel.app/'}${router.asPath}`;
-
-  useEffect(() => {
-    setPageTitle(document.title);
-  }, [router.asPath]);
 
   return (
     <ShareStyle className='share'>
-      <FacebookShareButton url={shareUrl} quote={pageTitle}>
+      <a target='_blank' href='https://www.facebook.com/ojoalclima'>
         <ReactSVG src='/icons/fb.svg' />
-      </FacebookShareButton>
-
-      <TwitterShareButton url={shareUrl} title={pageTitle}>
+      </a>
+      <a target='_blank' href='https://twitter.com/ojoalclima'>
         <ReactSVG src='/icons/x.svg' />
-      </TwitterShareButton>
-
-      <RedditShareButton url={shareUrl} title={pageTitle}>
-        <ReactSVG src='/icons/reddit.svg' />
-      </RedditShareButton>
-
-      <WhatsappShareButton url={shareUrl} title={pageTitle}>
-        <ReactSVG src='/icons/whats.svg' />
-      </WhatsappShareButton>
-    </ShareStyle>
+      </a>
+    </ShareStyle >
   );
 };
