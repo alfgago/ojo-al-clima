a55225c0876fab34a73c9634451ce5ff91e27af6
Fabricio Salgado <fabricio@5e.cr>
2024-03-11 18:53:52 -0600

    categories cache

--- src/pages/api/categories.ts
+++ src/pages/api/categories.ts
@@ -1,14 +1,21 @@
-import { customAPI } from "./base";
-import { queryClient } from "@/utils"; 
+import { customAPI } from "./base"
+import { queryClient } from "@/utils"
 
-export const fetchCategoryData = async (categoryName: string, page: number, staleTime?: number) => {
+export const fetchCategoryData = async (
+  categoryName: string,
+  page: number,
+  staleTime?: number
+) => {
   await queryClient.prefetchQuery(
-    ['categoryData', categoryName, page], 
+    ["categoryData", categoryName, page],
     async () => {
-      const response = await customAPI.get(`/categories/${categoryName}/${page}`);
-      return response.data;
-    }
-  );
+      const response = await customAPI.get(
+        `/categories/${categoryName}/${page}`
+      )
+      return response.data
+    },
+    { staleTime: staleTime ? staleTime * 1000 : undefined }
+  )
 
-  return queryClient.getQueryData(['categoryData', categoryName, page]);
-};
+  return queryClient.getQueryData(["categoryData", categoryName, page])
+}
